cb3407545448ee33d391f8672acd7656c866ac88
Roch Delsalle <rdelsalle@gmail.com>
2011-11-04 23:05:30 +0100

    adding the first model : user

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,9 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 import models.*;
@@ -12,5 +15,26 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
-
+    
+    public static class MD5Util {    
+	    public static String hex(byte[] array) {
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 0; i < array.length; ++i) {
+	            sb.append(Integer.toHexString((array[i]
+	                & 0xFF) | 0x100).substring(1,3));
+	        }
+	        return sb.toString();
+	    }
+	    public static String md5Hex (String message) {
+	        try {
+	            MessageDigest md =
+	                MessageDigest.getInstance("MD5");
+	            if(message!=null)
+	            return hex (md.digest(message.getBytes("CP1252")));
+	        } catch (NoSuchAlgorithmException e) {
+	        } catch (UnsupportedEncodingException e) {
+	        }
+	        return null;
+	    }
+	}
 }
---
+++ app/models/User.java
@@ -0,0 +1,136 @@
+package models;
+
+import siena.*;
+
+import controllers.Application;
+import controllers.Application.MD5Util;
+import play.libs.*;
+import play.data.validation.*;
+
+import com.google.gson.*;
+import play.cache.Cache;
+import java.util.List;
+import org.apache.commons.lang.WordUtils;
+import play.mvc.Scope.Session;
+import siena.embed.EmbedIgnore;
+import siena.embed.Embedded;
+import siena.embed.EmbeddedMap;
+
+@EmbeddedMap
+public class User extends Model {
+
+    @Id
+    public Long id;
+
+    public String openId;
+
+    @Email
+    public String email;
+
+    @Index("name")
+    public String name;
+
+    public String lang;
+
+    public String firstName;
+
+    public String lastName;
+    
+    public String twitter;
+    public Long twitterId;
+
+    public String facebook;
+
+    public String needConfirmation;
+
+    // ~~~~~~~~~~~~ 
+    
+    public User() {}
+    
+    public User(String openId, String email) {
+    	if(findByEmail(email)==null) {
+            this.openId = openId;
+            this.email = email;
+            this.needConfirmation = Codec.UUID();
+    	} 
+    }
+
+    public User(String email) {
+        if(findByEmail(email)==null) {
+            this.email = email;
+            this.needConfirmation = Codec.UUID();
+    	}
+    }
+    
+    public User(String twitter, Long twitterId) {
+        if(findByTwitterId(twitterId)==null) {
+            this.twitter = twitter;
+            this.twitterId = twitterId;
+            this.needConfirmation = Codec.UUID();
+    	}
+    }
+
+    public String fullName() {
+        return WordUtils.capitalize(this.name);
+    }
+
+    @Override
+    public String toString() {
+        if(this.twitter != null) {
+            return this.twitter;
+        } else {
+            return fullName();
+        }
+    }
+    // ~~~~~~~~~~~~ 
+    
+    static Query<User> all() {
+        return Model.all(User.class);
+    }
+
+    public static List<User> contacts() {
+        return all().fetch();
+    }
+
+    public static User findById(Long id) {
+        return all().filter("id", id).get();
+    }
+
+    public static User findByEmail(String email) {
+        return all().filter("email", email).get();
+    }
+
+    public static User findByTwitterId(Long twitterId) {
+        return all().filter("twitterId", twitterId).get();
+    }
+    
+    public static User findByName(String name) {
+        return all().filter("name", name).get();
+    }
+    
+    public static List<User> searchByName(String name) {
+        return all().search(name + "*", "name").fetch();
+    }
+
+    public static boolean isEmailAvailable(String email) {
+        return findByEmail(email) == null;
+    }
+
+    public String gravatar() {
+    	String hash = MD5Util.md5Hex(email);
+    	String url = "http://www.gravatar.com/avatar/";
+    	return url+hash;
+    }
+
+    public static void facebookOAuthCallback(JsonObject data){
+        String email = data.get("email").getAsString();
+        if(findByEmail(email)==null) {
+        	User user = new User(email);
+            user.insert();
+            Session.current().put("logged", user.id);
+        } else {
+        	User user = User.findByEmail(email);
+            Session.current().put("logged", user.id);
+        }
+    }
+}
